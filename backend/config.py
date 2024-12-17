7e1a30b7ca8ed8435e35c2820d508a7a4f06a6af
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-17 16:31:25 +0100

    - add database - env - User

--- backend/config.py
+++ backend/config.py
@@ -1,9 +1,15 @@
+from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "Awesome API"
+    app_name: str
     admin_email: str
     password_hash_secret_word: str
+    sqlite_file_name: str
 
     model_config = SettingsConfigDict(env_file=".env")
+
+@lru_cache
+def get_settings():
+    return Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
-from functools import lru_cache
 from typing import Annotated
-
-from fastapi import Depends, FastAPI
-from config import Settings
+from fastapi import Depends, FastAPI, Query
+from config import Settings, get_settings
+from db import create_db_and_tables, SessionDep
+from models.User import UserOut, UserIn, UserInDB, password_hasher
+from sqlmodel import select
 
 app = FastAPI()
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 
-@lru_cache
-def get_settings():
-    return Settings()
 @app.get("/info")
 async def info(settings: Annotated[Settings, Depends(get_settings)]):
     return {
@@ -20,3 +21,25 @@ async def info(settings: Annotated[Settings, Depends(get_settings)]):
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/users/")
+def create_user(user_in: UserIn, session: SessionDep) -> UserOut:
+    user_in_db = UserInDB(username=user_in.username, email=user_in.email,
+                          full_name=user_in.full_name,
+                          password=password_hasher(user_in.password))
+    session.add(user_in_db)
+    session.commit()
+    session.refresh(user_in_db)
+    return UserOut(**user_in_db.dict())
+
+@app.get("/users/")
+def read_users(
+        session: SessionDep,
+        offset: int = 0,
+        limit: Annotated[int, Query(le=100)] = 100,
+    ) -> list[UserOut]:
+    _users = session.exec(select(UserInDB)
+                         .offset(offset)
+                         .limit(limit)).all()
+    users = list(map(lambda user: UserOut(**user.dict()), _users))
+    return users
